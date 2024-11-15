f006866962c40ac9a801a7816584442eabf121c0
Gun-od Garig <gun-od.garig@elev.ga.ntig.se>
2024-11-15 10:06:11 +0100

    från igårs lektion

--- NäsanöverE01/Program.cs
+++ NäsanöverE01/Program.cs
@@ -5,25 +5,51 @@
 
 
 
-Console.WriteLine("Ange ditt användarnamn: ");
+// Console.WriteLine("Ange ditt användarnamn: ");
 
-Console.WriteLine("Ange ditt lösenord: ");
+// Console.WriteLine("Ange ditt lösenord: ");
 
-string username = Console.ReadLine();
-string password = Console.ReadLine();
+// string username = Console.ReadLine();
+// string password = Console.ReadLine();
 
-if (username == "kalleanka" && password == "12345")
+// if (username == "kalleanka" && password == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// else {
+//     Console.WriteLine("Wrong username or password");
+// }
+
+
+// int times = 0;
+
+// while (times < 32) 
+// {
+//     Console.WriteLine("Hello, World");
+//     times++;
+// }
+
+// Console.WriteLine("Skriv in ditt lösernord: ");
+// string password = Console.ReadLine();
+
+// while (password != "12345")
+// {
+//     password = Console.ReadLine();
+// }
+
+
+for ( int i = 0; i < 5; i++)
 {
-    Console.WriteLine("Welcome!");
-}
-else {
-    Console.WriteLine("Wrong username or password");
-}
- // när det körs så skriv det inte ut
-
-int times = 32;
-while (times > 32) {
-    Console.WriteLine("Hello, World");
-}
+    Console.WriteLine("Skriv in ditt tal: ");
+    
+    int number = int.TryParse
 
 
+    if (i > 5)
+    {
+        Console.WriteLine("Högre än 5!");
+    }
+}
+
+
+Console.ReadLine();
--- NäsanöverE01/obj/Debug/net8.0/NäsanöverE01.AssemblyInfo.cs
+++ NäsanöverE01/obj/Debug/net8.0/NäsanöverE01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NäsanöverE01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593fa37ad07990b5424093126160eb2f14f6fe86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46eb69ea54bde29636b5c7c0165479d6d53deb65")]
 [assembly: System.Reflection.AssemblyProductAttribute("NäsanöverE01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NäsanöverE01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
